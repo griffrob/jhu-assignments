7994b76ed4fd3152e624ec4f53c59644da658618
griffrob <33763602+griffrob@users.noreply.github.com>
2017-12-29 18:11:56 +0000

    Update script.js

--- module5/js/script.js
+++ module5/js/script.js
@@ -84,7 +84,8 @@ showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl,
   function (categories) {
-    buildAndShowHomeHTML (categories); // ***** <---- TODO: STEP 1: Substitute [...] ******
+    buildAndShowHomeHTML (categories); 
+  },// ***** <---- TODO: STEP 1: Substitute [...] ******
   true); // Explicitely setting the flag to get JSON from server processed into an object literal
 });
 // *** finish **
